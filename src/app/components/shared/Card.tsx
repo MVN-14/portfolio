e21e538d54f20f641b97bb1f48d0a433c2b76cd6
MVN-14 <mvn_14@outlook.com>
2023-09-28 15:39:32 -0400

    reverting last change

--- src/app/components/shared/Card.tsx
+++ src/app/components/shared/Card.tsx
@@ -1,8 +1,8 @@
 export function Card({ children }: PageProps) {
   return (
     <div
-      className="bg-black border-background border-8 bg-opacity-40 rounded-3xl"
-      style={{ overflowY: "auto", scrollBehavior: "smooth", height: "94vh" }}
+      className="bg-black border-background border-8 bg-opacity-40 rounded-3xl "
+      style={{ overflowY: "auto", scrollBehavior: "smooth", height: "9vh" }}
     >
       {children}
     </div>
