dc30d745cc310cd517be4b03773513c0e89cb3f3
MVN-14 <mvn_14@outlook.com>
2023-09-28 15:44:43 -0400

    added css calc for card height

--- src/app/components/shared/Card.tsx
+++ src/app/components/shared/Card.tsx
@@ -2,7 +2,11 @@ export function Card({ children }: PageProps) {
   return (
     <div
       className="bg-black border-background border-8 bg-opacity-40 rounded-3xl "
-      style={{ overflowY: "auto", scrollBehavior: "smooth", height: "9vh" }}
+      style={{
+        overflowY: "auto",
+        scrollBehavior: "smooth",
+        height: "calc(100vh - 3.5rem)",
+      }}
     >
       {children}
     </div>
