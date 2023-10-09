6fc253a411f80271929b80281cf753cfc95443cc
MVN-14 <117686444+MVN-14@users.noreply.github.com>
2023-10-09 14:08:31 -0400

    Added dynamic buttons to Project Component. Added cheome and edge store links to guitar tuner project.
    Added links prop to Project component to allow for more than just github button.

--- src/app/components/projects/Project.tsx
+++ src/app/components/projects/Project.tsx
@@ -1,77 +1,80 @@
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import githubSvg from "img/github-icon-white.png";
 import { Card } from "..";
 
 export function Project({
-  name,
-  image,
-  description,
-  githubLink,
-  tags,
+    name,
+    image,
+    description,
+    links,
+    tags,
 }: PageProps) {
-  return (
-    <Card>
-      <div className="lg:flex lg:justify-between px-5 py-2 border-background border-b-8">
-        <h1 className="my-auto text-2xl lg:text-4xl w-full lg:text-left">
-          {name}
-        </h1>
-        <h1 className="flex flex-row gap-2 justify-center">
-          {createTags(tags)}
-        </h1>
-      </div>
-      <div className="flex-col justify-between">
-        <Image
-          className="p-5 mx-auto"
-          src={image}
-          alt={`${name} image`}
-          style={{
-            width: "50%",
-            height: "auto",
-            minWidth: "350px",
-          }}
-        ></Image>
+    return (
+        <Card>
+            <div className="lg:flex lg:justify-between px-5 py-2 border-background border-b-8">
+                <h1 className="my-auto text-2xl lg:text-4xl w-full lg:text-left">
+                    {name}
+                </h1>
+                <h1 className="flex flex-row gap-2 justify-center">
+                    {createTags(tags)}
+                </h1>
+            </div>
+            <div className="flex-col justify-between">
+                <Image
+                    className="p-5 mx-auto"
+                    src={image}
+                    alt={`${name} image`}
+                    style={{
+                        width: "50%",
+                        height: "auto",
+                        minWidth: "350px",
+                    }}
+                ></Image>
+                <div className="flex justify-center gap-5">
+                    {links?.map(({ href, alt, text, logo }) =>
+                        <Link
+                            href={href}
+                            className="flex border-black bg-white text-black  w-28 md:w-36 text-xl md:p-2 rounded lg justify-around"
+                            target="_blank"
+                        >
+                            <Image
+                                src={logo}
+                                style={{
+                                    width: "35px",
+                                    height: "auto",
+                                }}
+                                alt={alt}
+                            />
+                            <p className="my-auto">{text}</p>
+                        </Link>
+                    )}
+                </div>
 
-        {githubLink && (
-          <Link
-            href={githubLink}
-            className="mx-auto flex border-black bg-white text-black  w-28 md:w-36 text-xl md:p-2 rounded lg justify-around"
-            target="_blank"
-          >
-            <Image
-              src={githubSvg}
-              style={{
-                width: "35px",
-                height: "auto",
-              }}
-              alt="Github Logo"
-            />
-            <p className="my-auto">Source</p>
-          </Link>
-        )}
-        <p className="py-5 sm:text-xl lg:text-2xl w-3/4 mx-auto text-sm">
-          {description}
-        </p>
-      </div>
-    </Card>
-  );
+                <p className="py-5 sm:text-xl lg:text-2xl w-3/4 mx-auto text-sm">
+                    {description}
+                </p>
+            </div>
+        </Card>
+    );
 }
 
 type PageProps = {
-  description: string;
-  image: StaticImageData;
-  githubLink?: string;
-  name: string;
-  tags: string[];
-};
+    description: string;
+    image: StaticImageData;
+    links?: Link[];
+    name: string;
+    tags: string[];
+}
+
+export type Link = { href: string, logo: StaticImageData, text: string, alt: string };
 
 function createTags(tags: string[]) {
-  return tags.map((tag) => (
-    <span
-      key={tag}
-      className="border lg:border-2 rounded-lg p-1 lg:p-2 text-sm lg:text-xl"
-    >
-      {tag}
-    </span>
-  ));
+    return tags.map((tag) => (
+        <span
+            key={tag}
+            className="border lg:border-2 rounded-lg p-1 lg:p-2 text-sm lg:text-xl"
+        >
+            {tag}
+        </span>
+    ));
 }
--- src/app/components/projects/Projects.tsx
+++ src/app/components/projects/Projects.tsx
@@ -1,46 +1,88 @@
 import spotifyTrackerImage from "img/spotify-tracker-top-artists.png";
 import tunerExtentionImage from "img/guitar-tuner.jpg";
 import portfolioImage from "img/portfolio.png";
+import githubLogo from "img/github-icon-white.png";
+import edgeLogo from "img/edge-logo.png";
+import chromeLogo from "img/chrome-logo.png";
 import { Carousel } from "react-responsive-carousel";
-import { Project } from "./Project";
+import { Link, Project } from "./Project";
+
 
 export function Projects() {
-  return (
-    <div className="text-center">
-      <Carousel showStatus={false} infiniteLoop showThumbs={false}>
-        <Project
-          description={spotifyTrackerDescription}
-          image={spotifyTrackerImage}
-          name="Spotify Tracker"
-          tags={["Angular", "RxJS", "Bootstrap", "Node", "Express"]}
-          githubLink="https://github.com/MVN-14/SpotifyTracker"
-        />
+    return (
+        <div className="text-center">
+            <Carousel showStatus={false} infiniteLoop showThumbs={false}>
+                <Project
+                    description={spotifyTrackerDescription}
+                    image={spotifyTrackerImage}
+                    name="Spotify Tracker"
+                    tags={["Angular", "RxJS", "Bootstrap", "Node", "Express"]}
+                    links={spotifyTrackerLinks}
+                />
 
-        <Project
-          description={tunerExtentionDescription}
-          image={tunerExtentionImage}
-          name="Tuner Extention"
-          tags={["JavaScript", "HTML", "ManifestV3"]}
-          githubLink="https://github.com/MVN-14/TunerExtension"
-        />
+                <Project
+                    description={tunerExtentionDescription}
+                    image={tunerExtentionImage}
+                    name="Tuner Extention"
+                    tags={["JavaScript", "HTML", "ManifestV3"]}
+                    links={tunerExtentionLinks}
+                />
 
-        <Project
-          description={portfolioDescription}
-          image={portfolioImage}
-          name="This Portfolio"
-          tags={["NextJS", "React", "TypeScript", "Tailwind"]}
-          githubLink="https://github.com/MVN-14/portfolio"
-        />
-      </Carousel>
-    </div>
-  );
+                <Project
+                    description={portfolioDescription}
+                    image={portfolioImage}
+                    name="This Portfolio"
+                    tags={["NextJS", "React", "TypeScript", "Tailwind"]}
+                    links={portfolioLinks}
+                />
+
+            </Carousel>
+        </div>
+    );
 }
 
 const spotifyTrackerDescription =
-  "Spotify Tracker is a project that lets spotify users interact with their personal data to see top songs/artists, recently played tracks, profile information, track/artist information, recommended tracks based on tracks or artists and more.";
+    "Spotify Tracker is a project that lets spotify users interact with their personal data to see top songs/artists, recently played tracks, profile information, track/artist information, recommended tracks based on tracks or artists and more.";
+const spotifyTrackerLinks: Link[] = [
+    {
+        href: "https://github.com/MVN-14/SpotifyTracker",
+        logo: githubLogo,
+        alt: "Github Logo",
+        text: "GitHub"
+    }
+];
+
 
 const tunerExtentionDescription =
-  "This is a web extention for Chrome and Edge browsers thats plays the acoustic notes of E Standard tuning to tune a guitar to standard tuning";
+    "This is a web extention for Chrome and Edge browsers thats plays the acoustic notes of E Standard tuning to tune a guitar to standard tuning";
+const tunerExtentionLinks: Link[] = [
+    {
+        href: "https://chrome.google.com/webstore/detail/standard-guitar-tuner/cbjcfjfglkgbcjpahfpnmmokkmbjfedl",
+        logo: chromeLogo,
+        alt: "Chrome Logo",
+        text: "Chrome"
+    },
+    {
+        href: "https://microsoftedge.microsoft.com/addons/detail/standard-guitar-tuner/dbnlmanoohhnpdpmepdekafdmkkiacon",
+        logo: edgeLogo,
+        text: "Edge",
+        alt: "Edge Logo"
+    },
+    {
+        href: "https://github.com/MVN-14/TunerExtension",
+        logo: githubLogo,
+        text: "GitHub",
+        alt: "Github Logo"
+    }
+];
 
 const portfolioDescription =
-  "This is a web portfolio built with NextJS 13 leveraging Tailwind CSS, Typescript, react-spring, and react-responsive-carousel libraries.";
+    "This is a web portfolio built with NextJS 13 leveraging Tailwind CSS, Typescript, react-spring, and react-responsive-carousel libraries.";
+const portfolioLinks = [
+    {
+        href: "https://github.com/MVN-14/portfolio",
+        logo: githubLogo,
+        alt: "GitHub Logo",
+        text: "GitHub"
+    }
+]
